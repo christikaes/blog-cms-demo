4e7c6c27b94d577c46147e5a2c393a79f9577c2e
christikaes <christikaes@gmail.com>
2018-08-13 01:34:18 -0400

    Update homepage post titles list from hardcoded data

--- index.php
+++ index.php
@@ -6,9 +6,23 @@
     <body>
         <?php include 'header.php' ?>
         <?php include 'nav.php' ?>
+        <?php
+            function getPostTitlesFromDatabase() {
+                // TODO in Module 4
+                // get this data from a database instead of hardcoding it
+                $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
+                return $postTitles;
+            }
+        ?>
         <main>
             <ul class="blog-list">
-                <li><a href="post.php">Blog Post 1</a></li>
+                <?php 
+                    $postTitles = getPostTitlesFromDatabase();
+
+                    foreach($postTitles as $postTitle) {
+                        echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a></li>";
+                    }
+                ?>
             </ul>
         </main>
         <?php include 'footer.php' ?>
